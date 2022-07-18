b294ab6e5a0d8a24d94c5029d19264e01ecb4ea7
Bobby McCann <bobby.mccann@spareroom.co.uk>
2022-07-18 23:21:49 +0100

    Converted them into chrono dates

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 use std::fs::{DirEntry};
-use chrono::DateTime;
+use chrono::{TimeZone, Utc};
 use regex::Regex;
 
 fn main() {
@@ -10,7 +10,10 @@ fn main() {
         let entry = entry.unwrap();
         let file_name = entry.file_name();
         for cap in date_re.captures_iter(file_name.to_str().unwrap()) {
-            println!("{}, {}, {}, {}, {}, {}", &cap[1], &cap[2], &cap[3], &cap[4], &cap[5], &cap[6]);
+            // println!("{}, {}, {}, {}, {}, {}", &cap[1], &cap[2], &cap[3], &cap[4], &cap[5], &cap[6]);
+            let date = Utc.ymd((&cap[1]).parse().unwrap(), (&cap[2]).parse().unwrap(), (&cap[3]).parse().unwrap())
+                .and_hms((&cap[4]).parse().unwrap(), (&cap[5]).parse().unwrap(), (&cap[6]).parse().unwrap());
+            println!("{}", date);
         }
         // println!("{}", entry.file_name().to_str().unwrap());
     }
