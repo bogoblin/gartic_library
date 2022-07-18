50a8a28c7e8dc7ea1b4fb649b9094966ac833f50
Bobby McCann <bobby.mccann@spareroom.co.uk>
2022-07-18 23:18:30 +0100

    Extracting the date numbers from the file names

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs;
+use std::fs::{DirEntry};
+use chrono::DateTime;
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let image_path = "/media/bobby/Big/Pictures/Pictures Classic/Gartic Phone";
+    let date_re = Regex::new(r"^album_(\d{4})-(\d{2})-(\d{2})_(\d{2})-(\d{2})-(\d{2})").unwrap();
+    for entry in fs::read_dir(image_path).unwrap() {
+        let entry = entry.unwrap();
+        let file_name = entry.file_name();
+        for cap in date_re.captures_iter(file_name.to_str().unwrap()) {
+            println!("{}, {}, {}, {}, {}, {}", &cap[1], &cap[2], &cap[3], &cap[4], &cap[5], &cap[6]);
+        }
+        // println!("{}", entry.file_name().to_str().unwrap());
+    }
 }
