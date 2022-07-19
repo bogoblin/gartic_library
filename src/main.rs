931f535ab34aae1776c834ce2e8adef60aa73a1b
Bobby McCann <bobby.mccann@spareroom.co.uk>
2022-07-19 02:30:30 +0100

    Export the gifs to files

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,35 @@
-use std::ffi::OsString;
 use std::fs;
-use chrono::{DateTime, TimeZone, Utc};
+use std::fs::File;
+use chrono::{Datelike, DateTime, Timelike, TimeZone, Utc};
 use regex::Regex;
+use std::path::{Path, PathBuf};
+use gif::{Frame};
 
 fn main() {
-    let image_path = OsString::from("/media/bobby/Big/Pictures/Pictures Classic/Gartic Phone");
+    let image_path = Path::new("/media/bobby/Big/Pictures/Pictures Classic/Gartic Phone");
+    let output_path = Path::new("/media/bobby/Big/Projects/Gartic Phone Static");
     let rounds = rounds_from_directory(image_path);
-    // for round in rounds {
-    //     println!("{}", round.date);
-    // }
     let games = games_from_rounds(rounds);
     for game in games {
-        println!("{} {} rounds", game.rounds.get(0).unwrap().image_path.to_str().unwrap(), game.rounds.len());
+        output_game(&output_path, game);
+    }
+
+}
+
+fn output_game(output_dir: &Path, game: Game) {
+    let game_dir = output_dir.join(game.dir());
+    for (i, round) in game.rounds.iter().enumerate() {
+        let round_dir = game_dir.join(format!("{:02}", i+1));
+        round.output_images(round_dir);
     }
 }
 
-fn rounds_from_directory(directory_path: OsString) -> Vec<Round> {
+fn rounds_from_directory(directory_path: &Path) -> Vec<Round> {
     let date_re = Regex::new(r"album_(\d{4})-(\d{2})-(\d{2})_(\d{2})-(\d{2})-(\d{2})\.gif$").unwrap();
     let mut rounds = Vec::new();
-    for entry in fs::read_dir(directory_path).unwrap() {
+    for entry in directory_path.read_dir().unwrap() {
         let entry = entry.unwrap();
-        let image_path = entry.path().into_os_string();
+        let image_path = entry.path();
         for cap in date_re.captures_iter(image_path.to_str().unwrap()) {
             let date = Utc.ymd((&cap[1]).parse().unwrap(), (&cap[2]).parse().unwrap(), (&cap[3]).parse().unwrap())
                 .and_hms((&cap[4]).parse().unwrap(), (&cap[5]).parse().unwrap(), (&cap[6]).parse().unwrap());
@@ -57,9 +66,57 @@ fn games_from_rounds(rounds: Vec<Round>) -> Vec<Game> {
 #[derive(Eq, Ord, PartialEq, PartialOrd, Clone)]
 struct Round {
     date: DateTime<Utc>,
-    image_path: OsString,
+    image_path: PathBuf,
+}
+
+impl Round {
+    fn output_images(&self, round_dir: PathBuf) {
+        let palette = &self.get_palette();
+
+        fs::create_dir_all(round_dir.as_path()).unwrap();
+        let file = File::open(&self.image_path).unwrap();
+        let mut options = gif::DecodeOptions::new();
+        options.set_color_output(gif::ColorOutput::RGBA);
+        println!("Processing {}", &self.image_path.to_str().unwrap());
+        let mut decoder = options.read_info(&file).unwrap();
+        let mut i = 0;
+        while let Some(frame) = decoder.read_next_frame().unwrap() {
+            let mut image = File::create(round_dir.join(format!("{:02}.gif", i+1))).unwrap();
+            i += 1;
+            let mut encoder = gif::Encoder::new(
+                &mut image,
+                frame.width,
+                frame.height,
+                &palette[..],
+            ).unwrap();
+            let new_frame = Frame::from_rgba(frame.width, frame.height, Vec::from(frame.buffer.clone()).as_mut_slice());
+            encoder.write_frame(&new_frame).unwrap();
+        }
+    }
+
+    fn get_palette(&self) -> Vec<u8> {
+        let file = File::open(&self.image_path).unwrap();
+        let mut options = gif::DecodeOptions::new();
+        options.set_color_output(gif::ColorOutput::RGBA);
+        let decoder = options.read_info(&file).unwrap();
+        Vec::from(decoder.global_palette().unwrap())
+    }
 }
 
 struct Game {
     rounds: Vec<Round>,
+}
+
+impl Game {
+    fn dir(&self) -> PathBuf {
+        let first_date = self.rounds.get(0).unwrap().date;
+        PathBuf::from(format!("{:04}/{:02}/{:02}/{:02}{:02}{:02}",
+                              first_date.year(),
+                              first_date.month(),
+                              first_date.day(),
+                              first_date.hour(),
+                              first_date.minute(),
+                              first_date.second(),
+        ))
+    }
 }
